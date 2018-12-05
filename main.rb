0359ce7fc0cbd078f93f296c16ae251ca04f407a
Saravanan Kothandapani <saravanan@codingmart.com>
2018-12-05 15:05:35 +0530

    Code done ! Except result display & 1 test case

--- main.rb
+++ main.rb
@@ -21,98 +21,117 @@ class Main
 		when 6
 			puts line_no + " Vistor is already inside the room"
 		when 7
-			puts line_no + " Vistor is already inside the room"
+			puts line_no + " OUT-time should be Greater IN-time or in_out time not present "
 		end
 	end
 
-	def self.except!(*keys)
-    keys.each { |key| delete(key) }
-    self
-  end
+	def self.room_present(data,line_number,visitor_id,room_no,action,time)
+		if data[room_no]["visitors"][visitor_id].present?
+			if action === "O"
+				data[room_no]["visitors"][visitor_id]["out"] = time
+				in_out = data[room_no]["visitors"][visitor_id]
+				if in_out["out"].present? && in_out["in"].present? && in_out["out"].to_i > in_out["in"].to_i
+					calculate_time = in_out["out"].to_i - in_out["in"].to_i
+					data[room_no]["time"] = data[room_no]["time"].to_i + calculate_time
+					data[room_no]["visited_users"].push(visitor_id)
+					data[room_no]["visited_users_count"] = data[room_no]["visited_users"].uniq.count
+					data[room_no]["avg_time"]= data[room_no]["time"].to_i / data[room_no]["visited_users_count"] 
+					data[room_no]["visitors"].delete(visitor_id)
+				else
+					Main.error_msg(7,line_number)
+					exit
+				end																			
+			else
+				Main.error_msg(6,line_number)
+				exit
+			end							
+		else
+			if action === "I" 
+				data[room_no]["visitors"][visitor_id] = {
+					"in" => time,
+					"out" => nil
+				}
+			else
+				Main.error_msg(5,line_number)
+				exit
+			end						
+		end
+		data			
+	end
 
+	def self.room_not_present(data,line_number,visitor_id,room_no,action,time)
+		visitor = Hash.new
+		visitor["time"] = 0
+		visitor["visited_users"] = []
+		visitor["avg_time"] = 0
+		visitor["visited_users_count"] = 0
+		visitor["visitors"] = Hash.new
+		if action === "I" 
+			visitor["visitors"][visitor_id] = {
+				"in" => time,
+				"out" => nil
+			}
+		else
+			Main.error_msg(5,line_number)
+			exit
+		end
+		data[room_no] = visitor
+		data
+	end
+
+	def self.get_input(n,data)
+		for i in 0..n-1 do
+			input = gets.chomp
+			input_array = input.split(" ")
+			line_number = i.to_s
+			if input_array.count === 4
+				visitor_id = input_array[0]
+				room_no = input_array[1]
+				action = input_array[2]
+				time = input_array[3]			
+				if visitor_id.scan(/\D/).empty? && visitor_id.to_i >=0 && visitor_id.to_i <= 1024
+					if room_no.scan(/\D/).empty? && room_no.to_i >= 0 && room_no.to_i <= 100
+						if action === "I" || action === "O"
+							if time.scan(/\D/).empty? && time.to_i>= 0 && time.to_i <= 1439 					
+								if data[room_no].present?
+									data = Main.room_present(data,line_number,visitor_id,room_no,action,time)
+								else
+									data = Main.room_not_present(data,line_number,visitor_id,room_no,action,time)
+								end
+							else
+								Main.error_msg(0,line_number)
+								break
+							end
+						else
+							Main.error_msg(1,line_number)
+							break
+						end				
+					else
+						Main.error_msg(2,line_number)
+						break
+					end
+				else
+					Main.error_msg(3,line_number)
+					break
+				end
+			else
+				Main.error_msg(4,line_number)
+				break
+			end
+		end
+		data
+	end
+
+	puts "Enter integer N which represents the following N-number of lines of text:"
 	n = gets.to_i
 
 	data = {}
 
-	for i in 0..n-1 do
-		input = gets.chomp
-		input_array = input.split(" ")
-		line_number = i.to_s
-		if input_array.count === 4
-			visitor_id = input_array[0]
-			room_no = input_array[1]
-			action = input_array[2]
-			time = input_array[3]			
-			if visitor_id.scan(/\D/).empty? && visitor_id.to_i >=0 && visitor_id.to_i <= 1024
-				if room_no.scan(/\D/).empty? && room_no.to_i >= 0 && room_no.to_i <= 100
-					if action === "I" || action === "O"
-						if time.scan(/\D/).empty? && time.to_i>= 0 && time.to_i <= 1439 					
-							if data[room_no].present?
-								if data[room_no]["visitors"][visitor_id].present?
-									if action === "O"
-										data[room_no]["visitors"][visitor_id]["out"] = time
-										in_out = data[room_no]["visitors"][visitor_id]
+	result = Main.get_input(n,data)
 
-										if in_out["out"].present? && in_out["in"].present? && in_out["out"].to_i > in_out["in"].to_i
-											calculate_time = in_out["out"].to_i - in_out["in"].to_i
-											data[room_no]["time"] = data[room_no]["time"].to_i + calculate_time
-											# data[room_no]["visitors"].except!(visitor_id)	
-											data[room_no]["visitors"].each { |key| delete(visitor_id) }
-										else
-											Main.error_msg(5,line_number)
-											break
-										end																			
-									else
-										Main.error_msg(6,line_number)
-										break
-									end							
-								else
-									if action === "I" 
-										data[room_no]["visitors"][visitor_id] = {
-											"in" => time,
-											"out" => nil
-										}
-									else
-										Main.error_msg(5,line_number)
-										break
-									end						
-								end								
-							else
-								visitor = Hash.new
-								visitor["time"] = 0
-								visitor["visited_users"] = []
-								visitor["visitors"] = Hash.new
-								if action === "I" 
-									visitor["visitors"][visitor_id] = {
-										"in" => time,
-										"out" => nil
-									}
-								else
-									Main.error_msg(5,line_number)
-									break
-								end
-								data[room_no] = visitor
-							end
-						else
-							Main.error_msg(0,line_number)
-							break
-						end
-					else
-						Main.error_msg(1,line_number)
-						break
-					end				
-				else
-					Main.error_msg(2,line_number)
-					break
-				end
-			else
-				Main.error_msg(3,line_number)
-				break
-			end
-		else
-			Main.error_msg(4,line_number)
-			break
-		end
+	if result.present?
+		ordered_result = result.sort_by{|key| key}
+		binding.pry
 	end
-
+	
 end
